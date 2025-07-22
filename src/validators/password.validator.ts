aff31bec987217929071c8cae1532580be6db1b5
Alireza Aberoumand <maya7192@yahoo.com>
2025-07-21 19:34:28 -0700

    Refactor password validation schema in password.validator.ts for improved clarity
    Updated the password validation schema to use an object structure, enhancing the clarity of validation messages. Each validation rule now provides more specific feedback, ensuring users understand the requirements for a valid password.

--- src/validators/password.validator.ts
+++ src/validators/password.validator.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
-const passwordSchema = z
-    .string()
-    .min(8, 'Password must be at least 8 characters')
-    .regex(/[a-z]/, 'Password 1 lowercase letter required')
-    .regex(/[A-Z]/, '1 uppercase letter required')
-    .regex(/[0-9]/, '1 digit required')
-    .regex(/[!@#$%^&*(),.?":{}|<>]/, '1 special character required');
+const passwordSchema = z.object({
+    password: z
+        .string()
+        .min(8, 'Password must be at least 8 characters long')
+        .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+        .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+        .regex(/[0-9]/, 'Password must contain at least one number')
+        .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character'),
+});
 
 export { passwordSchema };
